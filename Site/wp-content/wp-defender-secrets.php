40f23908e6308d67a8f0aeb33859d58752903e59
Apple_163 <116505603+Apple-User-163@users.noreply.github.com>
2023-07-16 12:12:49 +0530

    Delete Site directory

--- Site/wp-content/wp-defender-secrets.php
+++
@@ -1,8 +0,0 @@
-<?php
-// The file is used to generate and validate secret keys. Deleting this file or manually editing the content will permanently break 2fa TOTP method for all users.
-if ( ! defined( 'WPINC' ) ) {
-	die;
-}
-// WP Defender - Start
-define( 'WP_DEFENDER_TOTP_KEY', '2+wZiZSo8wk86BlsVpQFxD+Q+yik0cANjltXYbox6h0=' );
-// WP Defender - End
