a74b3f07ca0a3d3d714373007bc9b0af51f26784
Apple_163 <116505603+Apple-User-163@users.noreply.github.com>
2023-07-16 11:42:07 +0530

    Add files via upload

---
+++ Site/wp_appleuser163.ezyro.com_2023-07-16_05-53-21.php
@@ -0,0 +1,14 @@
+<?php exit();?>
+{
+    "name": "wp_appleuser163.ezyro.com_2023-07-16_05-53-21",
+    "backup_dir": "1",
+    "backup_db": "1",
+    "email": "avinumathew@gmail.com",
+    "date_time": "2023-07-16 05:58:am",
+    "btime": 1689487104,
+    "auto_backup": false,
+    "ext": "tar.gz",
+    "size": false,
+    "backup_site_url": "http:\/\/appleuser163.ezyro.com",
+    "backup_site_path": "\/home\/vol16_1\/ezyro.com\/ezyro_33321395\/htdocs"
+}
